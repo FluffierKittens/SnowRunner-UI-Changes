5abc1aafe47fcb22afbbb1484e99ac3b15eef0ce
FluffierKittens <130815166+FluffierKittens@users.noreply.github.com>
2024-02-01 04:44:56 -0800

    Updating for new version

--- DLL/SRUIC.cpp
+++ DLL/SRUIC.cpp
@@ -59,7 +59,7 @@ void Init()
     freopen_s(&file, "CONOUT$", "w", stderr);
 #endif
 
-    CONSOLE_LOG("SnowRunner UI Changes v0.1.3")
+    CONSOLE_LOG("SnowRunner UI Changes v0.1.4")
 
 	//Set our hook
     DetourRestoreAfterWith();
