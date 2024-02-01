b1c0f9dc77f542445d5ee4205441691a0848c994
FluffierKittens <130815166+FluffierKittens@users.noreply.github.com>
2024-02-01 04:47:51 -0800

    Updating for compatibility with Season 12

--- DLL/include/data_types.h
+++ DLL/include/data_types.h
@@ -2,9 +2,9 @@
 
 class ControllerState {
 public:
-	char pad_000[2904];			//0x000					
+	char pad_000[2904];		//0x000					
 	XINPUT_GAMEPAD X360;		//0xB58 
-	char pad_B64[8];			//0xB64 
+	char pad_B64[8];		//0xB64 
 	DWORD dGetStateSuccess;		//0xB6C 
 };
 
@@ -21,10 +21,10 @@ public:
   bool AWD;                   // 0x0049
   bool Diff;                  // 0x004A
   char pad_004B[21];          // 0x004B
-  uint64_t N0000005B; 		  // 0x0060
+  uint64_t N0000005B;         // 0x0060
   char pad_0068[8];           // 0x0068
   int32_t Gear_1;             // 0x0070
-  int32_t Gear_2;        	  // 0x0074
+  int32_t Gear_2;             // 0x0074
   char pad_0078[56];          // 0x0078
   float N00000065;            // 0x00B0
   float N00000319;            // 0x00B4
@@ -33,47 +33,47 @@ public:
   char pad_00C0[24];          // 0x00C0
   float N0000006A;            // 0x00D8
   float SwitchThreshold;      // 0x00DC
-  int32_t NextGear;      	  // 0x00E0
+  int32_t NextGear;           // 0x00E0
   char pad_00E4[284];         // 0x00E4
 };                          
 
 class Vehicle {
 public:
   char pad_0000[128];						// 0x0000
-  class TruckAction * pTruckAction;			// 0x0080
-  char pad_0088[1760];						// 0x0088
-  char cEngineOnAndMore;					// 0X0768 
-  char pad_0769[111];						// 0x0769
-  Vehicle * pTrailer;						// 0x07D8
-  char pad_7E0[208];						// 0x07E0
-  uint64_t pWinchConstraint;				// 0x08B0 
-  char pad_08B8[64];						// 0x08B8 
-  bool bWinchNeedsEngine;					// 0x08F8 
+  class TruckAction * pTruckAction;				// 0x0080
+  char pad_0088[1776];						// 0x0088
+  char cEngineOnAndMore;					// 0X0778 
+  char pad_0769[111];						// 0x0779
+  Vehicle * pTrailer;						// 0x07E8
+  char pad_7E0[208];						// 0x07F0
+  uint64_t pWinchConstraint;					// 0x08C0 
+  char pad_08B8[64];						// 0x08C8 
+  bool bWinchNeedsEngine;					// 0x0908 
 };
 
 class DriveLogic
 {
 public:
-	char pad_0000[32]; 				//0x0000
+	char pad_0000[32]; 		//0x0000
 	class Vehicle * pCurVehicle; 	//0x0020
-	char pad_0028[353]; 			//0x0028
-	char bWinchConnect;   			//0x0189
-	char bWinchView; 				//0X018A 
+	char pad_0028[353]; 		//0x0028
+	char bWinchConnect;   		//0x0189
+	char bWinchView; 		//0X018A 
 }; 
 
 class TruckControl {
 public:
-  char pad_0000[8];                 // 0x0000
-  class Vehicle *pCurVehicle;       // 0x0008
-  char pad_0010[28];                // 0x0010  
-  bool bPauseOrMap;					// 0x002C
-  char pad_002D[67];			    // 0x002D
-  uint64_t N0000094C; 				// 0x0070 //combine::SOUND_OBJECT
-  uint64_t N0000094D; 				// 0x0078 //combine::SOUND_OBJECT
-  uint64_t N0000094E; 				// 0x0080 //combine::SOUND_OBJECT
-  char pad_0088[8];                 // 0x0088
-  class String *N00000950;          // 0x0090
-  char pad_0098[64];                // 0x0098 
-  bool bFunctionMenu;				// 0x00D8 			
-  bool bFastMode;					// 0x00D9  
+  char pad_0000[8];                 	// 0x0000
+  class Vehicle *pCurVehicle;       	// 0x0008
+  char pad_0010[28];                	// 0x0010  
+  bool bPauseOrMap;			// 0x002C
+  char pad_002D[67];			// 0x002D
+  uint64_t N0000094C; 			// 0x0070 //combine::SOUND_OBJECT
+  uint64_t N0000094D; 			// 0x0078 //combine::SOUND_OBJECT
+  uint64_t N0000094E; 			// 0x0080 //combine::SOUND_OBJECT
+  char pad_0088[8];                 	// 0x0088
+  class String *N00000950;          	// 0x0090
+  char pad_0098[64];               	// 0x0098 
+  bool bFunctionMenu;			// 0x00D8 			
+  bool bFastMode;			// 0x00D9  
 };                                                              
